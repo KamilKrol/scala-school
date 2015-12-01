1af538e6d3edaf98b0fbe6ce05fe9b7059fd90a6
Kamil Krol <kamil.h.krol@gmail.com>
2015-12-01 14:17:14 +0100

    Advanced pattern matching.

---
+++ 2-basics-continued/src/Main6.scala
@@ -0,0 +1,30 @@
+
+
+/**
+  * Created by kkrol on 01/12/2015.
+  */
+object Main6 {
+
+  def main(args: Array[String]): Unit = {
+    val calc1 = new Calculator("HP", "20B")
+    val calc2 = new Calculator("HP", "48G")
+    val calc3 = new Calculator("HP", "30B")
+    val calc4 = new Calculator("HP", "XXX")
+    val calc5 = new Calculator("XXX", "30B")
+
+    val list = List(calc1, calc2, calc3, calc4, calc5)
+    val mappedList = list.map(matchFunction)
+    println(list)
+    println(mappedList)
+  }
+
+  def matchFunction(calc: Calculator) = calc match {
+    case _ if calc.brand == "HP" && calc.model == "20B" => "financial"
+    case _ if calc.brand == "HP" && calc.model == "48G" => "scientific"
+    case _ if calc.brand == "HP" && calc.model == "30B" => "business"
+    case _ => "unknown = " + calc
+  }
+
+}
+
+case class Calculator(brand: String, model: String)
