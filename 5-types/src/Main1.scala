7a8c8279e4f1e4faa5534f3bad35f765e5f21404
KamilKrol <kamil.h.krol@gmail.com>
2015-12-02 22:42:34 +0100

    chapter 5 - on-going... to be continued, i need more studying on covariant/contracovariant functions.

--- 5-types/src/Main1.scala
+++ 5-types/src/Main1.scala
@@ -4,7 +4,14 @@
 object Main1 {
 
   def main(args: Array[String]): Unit = {
-    println("XXX")
+
+    val list = List(1, 2, 3, 4, 5)
+
+    val droppedList = drop1(list)
+    println("droppedList:  " + droppedList)
+
   }
+
+  def drop1[T](list: List[T]) = list.tail
 
 }
