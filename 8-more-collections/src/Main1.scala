2f4f11c213814c1cb4a40a70d7e578bb3ec8e16a
Kamil Krol <kamil.h.krol@gmail.com>
2015-12-04 08:23:16 +0100

    More collections.

---
+++ 8-more-collections/src/Main1.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by kkrol on 04/12/2015.
+  */
+object Main1 {
+
+  def main(args: Array[String]): Unit = {
+
+    val list1 = List(1, 2, 3, 4, 5)
+    println(list1)
+
+    val list2 = 1::2::3::4::5::Nil
+    println(list2)
+
+  }
+
+}
